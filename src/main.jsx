b3201ebad69bfa2af4b94ba02d6dcbdb42a264b8
SadlyItadakimasu <guikumagai@gmail.com>
2024-10-08 15:26:59 -0300

    organizando-elementos-3d

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,25 @@
 
-  import { createRoot } from 'react-dom/client'
-  import React, { StrictMode, useState } from 'react';
-  import './styles/global.css'
-  import {Home} from './pages/Home'
-  import { Terrain } from './pages/Home/index' 
-  import { NightSystem } from './pages/Home/index';
+import { createRoot } from 'react-dom/client'
+import React, { StrictMode, useState } from 'react';
+import './styles/global.css'
+import { Home } from './pages/Home'
+
+import { NightSystem } from './pages/Home/index';
+
 
 function App() {
-  const [field, setField] = useState(new Map());
 
-  
   return (
-    <div className='flex-container'>
-      <NightSystem/>
-      <Terrain field={field} /> {}
-      <Home field={field} setField={setField} /> {}
+    <div>
+      <NightSystem />
+      <Home />
     </div>
   );
+  
 }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App /> {}
+    <App /> { }
   </StrictMode>,
 );
